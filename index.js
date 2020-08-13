823c2fd1d492ec22a3250257a44b085af0a18520
kurosame <kurosame1227@gmail.com>
2020-08-13 11:06:07 +0900

    add: extends prettier

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ module.exports = {
   extends: [
     '@kurosame/eslint-config-vue',
     '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/vue'
   ],
   settings: {
     'import/core-modules': ['vue']
